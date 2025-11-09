6104c09156a12953aa83f29121e9bb5f8d0ec1fa
Saathik-Basha <saathik.basha2022@vitstudent.ac.in>
2025-11-09 17:10:16 +0530

    Changesv3

--- functions/updatePhoto.js
+++ functions/updatePhoto.js
@@ -1,5 +1,6 @@
 const parser = require("lambda-multipart-parser");
-import { v4 as uuidv4 } from 'uuid';
+const { v4: uuidv4 } = require('uuid');
+
 
 const sharp = require("sharp");
 const sendResponse = require("../utils/sendResponse");
