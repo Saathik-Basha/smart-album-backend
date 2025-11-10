87837d2bab7490771efecdb4a4a84cd88a5227b6
Saathik-Basha <saathik.basha2022@vitstudent.ac.in>
2025-11-10 19:15:51 +0530

    Added Security

--- const/providers.js
+++ const/providers.js
@@ -5,9 +5,11 @@ const rekognition = new AWS.Rekognition({
   region: process.env.AWS_REGION || "us-east-1",
 });
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const cognito = new AWS.CognitoIdentityServiceProvider();
 
 module.exports = {
     s3,
     rekognition,
     dynamoDb,
+    cognito,
 };
---
+++ functions/register.js
@@ -0,0 +1,53 @@
+const sendResponse = require("../utils/sendResponse");
+const {
+  PHOTOS_TABLE,
+  ORIGINAL_BUCKET_NAME,
+  THUMBNAIL_BUCKET_NAME,
+  USER_POOL,
+} = require("../const/paths");
+
+const { s3, rekognition, dynamoDb, cognito } = require("../const/providers");
+module.exports.register = async (event) => {
+  try {
+    console.log("require('../const/paths'):", require("../const/paths"));
+    console.log("process.env.USER_POOL:", process.env.USER_POOL);
+
+    console.log({ USER_POOL });
+    const { email, password } = JSON.parse(event.body);
+
+    console.log({ email, password });
+    const result = await cognito
+      .adminCreateUser({
+        UserPoolId: USER_POOL,
+        Username: email,
+        UserAttributes: [
+          {
+            Name: "email",
+            Value: email,
+          },
+          {
+            Name: "email_verified",
+            Value: "true",
+          },
+        ],
+        MessageAction: "SUPPRESS",
+      })
+      .promise();
+
+    if (result.User) {
+      await cognito
+        .adminSetUserPassword({
+          Password: password,
+          UserPoolId: USER_POOL,
+          Username: email,
+          Permanent: true,
+        })
+        .promise();
+    }
+
+    return sendResponse(200, { result });
+  } catch (error) {
+    console.error(error);
+    return sendResponse(400, error);
+  }
+};
